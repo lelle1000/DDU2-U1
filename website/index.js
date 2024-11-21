7e7a92b305d22bc96010eb8da48cc34aff09ec43
lelle1000 <linusestrandberg@gmail.com>
2024-11-21 22:38:22 +0100

    Lagt till script, och länkat css.
    Har testat mig fram lite för att se hur uppgiften ska utföras även om svaren inte stämmer så är det nyttigt att försöka.

--- website/index.js
+++ website/index.js
@@ -1,6 +1,22 @@
 // Recommended: All functions declared here
 
 // Recommended: constants with references to existing HTML-elements
+const main = document.querySelector("main");
+main.className = main;
+
+
+
+const h2 = document.querySelector("h2");
+
+let WhatCity = prompt("Vilken stad?")
+if (WhatCity == cities.name) {
+    h2.textContent = `${cities.name} (${cities.country})`
+    document.title = `${WhatCity}`
+} else 
+    h2.textContent = `${WhatCity} finns inte i databasen`
+    document.title = "Not Found";
+
+
 
 // Recommended: Ask for the city name and then the rest of the code
 
