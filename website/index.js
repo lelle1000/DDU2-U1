4c8a85c9e03c553f5b3b261f451dff82fd6e10ef
lelle1000 <linusestrandberg@gmail.com>
2024-11-22 18:32:59 +0100

    Fick databasen att fungera samt löste frågan om staden.
    Fick pilla en del men fick tillslut in en funktion som ser till att om värdet som man skriver i prompten inte hittas i cities arrayen så ska loopen ha avslutats och h2's content ska ha ändrats till promptens namn, och därefter finns inte i databasen.

--- website/index.js
+++ website/index.js
@@ -2,20 +2,33 @@
 
 // Recommended: constants with references to existing HTML-elements
 const main = document.querySelector("main");
-main.className = main;
-
-
 
 const h2 = document.querySelector("h2");
 
-let WhatCity = prompt("Vilken stad?")
-if (WhatCity == cities.name) {
-    h2.textContent = `${cities.name} (${cities.country})`
-    document.title = `${WhatCity}`
-} else 
-    h2.textContent = `${WhatCity} finns inte i databasen`
-    document.title = "Not Found";
 
+
+
+
+// Recommended: Ask for the city name and then the rest of the code
+main.className = main;
+
+let WhatCity = prompt("Vilken stad?")
+
+function findCity(city) {
+
+    for (let i = 0; i < cities.length; i++) {
+        if (city === cities[i].name) {
+            h2.textContent = `${cities[i].name} (${cities[i].country})`
+            document.title = `${WhatCity}`
+
+            return;
+        } 
+    }
+        h2.textContent = `${WhatCity} finns inte i databasen`
+        document.title = "Not Found";
+}
+   
+    findCity(WhatCity)
 
 
 // Recommended: Ask for the city name and then the rest of the code
