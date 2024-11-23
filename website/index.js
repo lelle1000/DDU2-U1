319372d5389b271d18a23308dc41769df633e408
Linus Strandberg <linusestrandberg@gmail.com>
2024-11-23 16:38:26 +0100

    Påbörjat andra sektionen i huvuddelen av hemsidan.
    Hittat ett sätt som gör funktionen lowercase-säker så även om man skriver graz ska den hitta.
    
    Har även påbörjat en enkel loop som skapar städerna nedretill som jag nu ska börja få till hur man highlitar den valda staden med färg.

--- website/index.js
+++ website/index.js
@@ -1,15 +1,13 @@
-// Recommended: All functions declared here
 
-// Recommended: constants with references to existing HTML-elements
 const main = document.querySelector("main");
 
-const h2 = document.querySelector("h2");
+const CityOfChoice = document.querySelector("h2");
 
 
 
 
 
-// Recommended: Ask for the city name and then the rest of the code
+
 main.className = main;
 
 let WhatCity = prompt("Vilken stad?")
@@ -17,19 +15,25 @@ let WhatCity = prompt("Vilken stad?")
 function findCity(city) {
 
     for (let i = 0; i < cities.length; i++) {
-        if (city === cities[i].name) {
-            h2.textContent = `${cities[i].name} (${cities[i].country})`
+        if (city.toLowerCase() === cities[i].name.toLowerCase()) {
+            CityOfChoice.textContent = `${cities[i].name} (${cities[i].country})`
             document.title = `${WhatCity}`
 
             return;
         } 
     }
-        h2.textContent = `${WhatCity} finns inte i databasen`
+        CityOfChoice.textContent = `${WhatCity} finns inte i databasen`
         document.title = "Not Found";
 }
-   
     findCity(WhatCity)
 
 
-// Recommended: Ask for the city name and then the rest of the code
+
+for (let i = 0; i < cities.length; i++) {
+    document.getElementById("cities").innerHTML += `<div class="cityBox">${cities[i].name}</div>`
+}
+
+
+
+
 
