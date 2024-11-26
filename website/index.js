a4e4d63ae84bacb0fd66fc99ce94867756c18e47
Linus Strandberg <linusestrandberg@gmail.com>
2024-11-26 15:18:52 +0100

    Lagt till så lådan blir svart som skrivs in i prompten

--- website/index.js
+++ website/index.js
@@ -5,19 +5,19 @@ const CityOfChoice = document.querySelector("h2");
 
 
 
-
-
-
-main.className = main;
-
+// Ställer frågan vilken stad
 let WhatCity = prompt("Vilken stad?")
 
 function findCity(city) {
+
+    const AddTarget = document.getElementsByClassName("cityBox")
 
     for (let i = 0; i < cities.length; i++) {
         if (city.toLowerCase() === cities[i].name.toLowerCase()) {
             CityOfChoice.textContent = `${cities[i].name} (${cities[i].country})`
             document.title = `${WhatCity}`
+            
+            AddTarget[i].classList.add("target")
 
             return;
         } 
@@ -25,15 +25,9 @@ function findCity(city) {
         CityOfChoice.textContent = `${WhatCity} finns inte i databasen`
         document.title = "Not Found";
 }
-    findCity(WhatCity)
-
-
 
 for (let i = 0; i < cities.length; i++) {
     document.getElementById("cities").innerHTML += `<div class="cityBox">${cities[i].name}</div>`
+    
 }
-
-
-
-
-
+findCity(WhatCity)
